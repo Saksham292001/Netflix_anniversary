b5deea19e5824052da5754ebdcd998aea90d271f
Saksham292001 <github.com>
2026-06-14 17:43:53 +0530

    added env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// We add https:// and .supabase.co around your Project ID
-const supabaseUrl = 'https://geyuwjvyxzhgptrwixvc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdleXV3anZ5eHpoZ3B0cndpeHZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzOTgwMzcsImV4cCI6MjA5Mzk3NDAzN30.Qzi-X4ahlzZNdqp9qlkXU50oZ5AiMNRe144qbgZZOSs';
+// Vite uses import.meta.env to read variables from the .env file
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
